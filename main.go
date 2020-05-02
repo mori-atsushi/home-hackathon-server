b6805c1d7f5174d5c2466f4f51a635e31eb5c6d6
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-02 18:28:05 +0900

    Implement a ping method

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"./pb"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const (
+	port = ":8080"
+)
+
+type server struct{}
+
+func (s *server) Ping(ctx context.Context, req *pb.PingRequest) (*pb.PingResponse, error) {
+	log.Printf("Received: %v", req)
+	return &pb.PingResponse{Message: "pong"}, nil
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterAppServiceServer(s, &server{})
+	reflection.Register(s)
+	log.Printf("Listening on %v", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
